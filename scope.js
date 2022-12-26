2c497779fc60fff5d1d60d20d570432417a84449
dinislamgazi4 <mddinislamgazi8027@gmail.com>
2022-12-26 15:32:54 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,24 @@
+let bonus = 20;
+
+
+function sum ( first, second){
+    let result = first + second + bonus;
+    console.log(bonus);
+    if(result > 9 ){
+         const mood = "happy";
+         console.log(mood);
+
+        var mood = "happy";
+         mood = "first"
+         mood ="fuky"
+         mood = "coky"
+         console.log(mood)
+    }
+    var day = "friend"
+    console.log(day)
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
