11f40bea778939e3da2bd3691694bfacd4846a85
dinislamgazi4 <mddinislamgazi8027@gmail.com>
2022-12-26 09:27:51 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// 0;
+// "";
+// udenfined
+// null
+// NaN
+// '00'
+
+let name = null;
+console.log(name);
+
+
+if(name){
+    console.log("condition is true")
+}
+
+else{
+    console.log("condition is false")
+}
